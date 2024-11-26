fd182111af8d926a5083a86ab2be369f5b325121
Adityatitu <Adityatitu01@gmail.com>
2024-11-27 05:14:02 +0530

    production debugging

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const cors = require('cors')
 //<--------------------------------------CORS DEFINITION------------------------------------------------->
 
 var corsOptions = {
-    origin: 'https://festify-ease.netlify.app',
+    origin: 'http://localhost:5173',
     methods:"GET, POST,PUT,DELETE, PATCH, HEAD",
     optionsSuccessStatus: 200,
     Credentials:true,
