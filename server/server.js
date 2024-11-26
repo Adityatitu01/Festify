94458dd66fd07a7103a818110e25b06b3e0581ad
Adityatitu <Adityatitu01@gmail.com>
2024-11-27 04:17:28 +0530

    deployment

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const cors = require('cors')
 //<--------------------------------------CORS DEFINITION------------------------------------------------->
 
 var corsOptions = {
-    origin: 'https://astounding-piroshki-15baea.netlify.app/',
+    origin: 'http://localhost:5173',
     methods:"GET, POST,PUT,DELETE, PATCH, HEAD",
     optionsSuccessStatus: 200,
     Credentials:true,
